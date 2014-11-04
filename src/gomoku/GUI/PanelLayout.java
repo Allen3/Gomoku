01cfd9d7093a4e1d9fa87bd57f9edfc567df763c
Allen3 <Allen.3.ysj@gmail.com>
2014-11-04 17:30:46 +0800

    Reset the auto merge by mistake

---
+++ src/gomoku/GUI/PanelLayout.java
@@ -0,0 +1,44 @@
+package gomoku.GUI;
+
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.shape.Circle;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author allen
+ */
+public class PanelLayout {
+        
+    private final Parent root;
+    
+    private final int numOfRows;
+    private final int numOfColumns;
+    private final int gridInterval;
+    
+    private Desk desk;
+
+    public PanelLayout() {
+        this.numOfRows = 10;
+        this.numOfColumns = 10;
+        this.gridInterval = 50;
+        
+        desk = new Desk(numOfRows, numOfColumns, gridInterval);        
+        
+        this.root = desk;
+    }   //panel()        
+
+    public Parent getRoot() {
+        return root;
+    }   //getRoot()           
+    
+    
+}   //PanelLayout
